3e2d3b05dad76b7a246f24aee8de3a10a0901e1b
limsatgat <96382179+limsatgat@users.noreply.github.com>
2022-10-19 01:29:26 +0900

    chapter 07

---
+++ ch07/src/Ex01_Tv.java
@@ -0,0 +1,11 @@
+class Tv {
+    boolean power;
+    int channel;
+
+    void power(){ power = !power; }
+    void channelUp(){ ++channel; }
+    void channelDown(){ --channel;}
+}
+
+public class Ex01_Tv {
+}
