ca1425ee0fa92b73138cc5ae68576a3d1e9238bf
suheon.lim <suheon.lim@lemonhc.com>
2022-11-09 18:02:44 +0900

    training

--- swingTraining/src/Swing.java
+++ swingTraining/src/Swing.java
@@ -9,27 +9,31 @@ import java.awt.event.ActionListener;
 public class Swing extends JFrame {
     public Swing() {
         setTitle("발전하는 임스윙스");
-        setSize(1200, 800);
+        setSize(600, 400);
 
         Container c = getContentPane();
         c.setLayout(new FlowLayout());
 
+        // 복사된 문자 저장
+
 
         // 텍스트 입력창
-        JTextField inputArea = new JTextField("", 100);
+        JTextField inputArea = new JTextField("", 50);
 
         JPanel button = new JPanel();
 
-        // 복사버튼
+        // 복사버튼을 누르면 inputArea의 값이 담김
         JButton jClone = new JButton("복사");
         jClone.addActionListener(e -> {
             inputArea.getText();
         });
 
+        String textArea = inputArea.getText();
+
         // 붙여넣기
         JButton jPaste = new JButton("붙여넣기");
         jPaste.addActionListener(e -> {
-//            inputArea.setText(inputArea);
+            inputArea.setText(textArea);
         });
 
         // 삭제버튼
