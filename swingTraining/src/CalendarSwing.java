875ef0284b9f79cced6e66ca359686c3001476d9
suheon.lim <suheon.lim@lemonhc.com>
2022-11-21 16:23:58 +0900

    SwingCalendar

---
+++ swingTraining/src/CalendarSwing.java
@@ -0,0 +1,72 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.Calendar;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class CalendarSwing extends JFrame implements ItemListener, ActionListener {
+
+
+
+    // 생성자
+    public CalendarSwing(){
+
+    }
+
+    // 날짜 세팅
+    public void setDay(){
+
+    }
+
+    // 요일 세팅
+    public void setCalendarTitle(){
+
+    }
+
+    // 년도 세팅
+    public void setYear(){
+
+    }
+
+    // 월 세팅
+    public void setMonth(){
+
+    }
+
+    // 날짜설정 콤보박스 이벤트
+    public void itemStateChanged(ItemEvent e){
+
+    }
+
+    // 날짜이동 버튼 이벤트
+    public void actionPerformed(ActionEvent ae){
+
+    }
+
+    // 콤보박스로 년/월 설정시 초기화
+    private void setDayReset(){
+
+    }
+
+    // 월 이동(다음으로)
+    public void nextMonth(){
+
+    }
+
+    // 월 이동(이전으로)
+    public void prevMonth(){
+
+    }
+
+    public static void main(String[] args) {
+        new CalendarSwing();
+    }
+}
